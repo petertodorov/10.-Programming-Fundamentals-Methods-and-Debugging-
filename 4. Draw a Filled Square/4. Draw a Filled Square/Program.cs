0877ab67813e1d32b4420acd1a9179fc95eb2a5a
petertodorov <petodorov@gmail.com>
2017-06-10 10:39:25 +0300

    E 4

---
+++ 4. Draw a Filled Square/4. Draw a Filled Square/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Draw_a_Filled_Square
+{
+    class Draw_a_Filled_Square
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            DrawStraightLine(n);
+            DrawMiddlePart(n);
+            DrawStraightLine(n);
+        }
+
+        private static void DrawMiddlePart(int n)
+        {
+            for (int j = 1; j < n - 1; j++)
+            {
+                Console.Write("-");
+                for (int i = 0; i <= n - 2; i++)
+                {
+                    Console.Write("\\/");
+                }
+                Console.WriteLine("-");
+            }            
+        }
+
+        private static void DrawStraightLine(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+        }
+    }
+}
