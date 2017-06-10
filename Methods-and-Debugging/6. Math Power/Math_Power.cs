92d25808dfd6e6ae88cc7cd0f5da0b274fc625b9
petertodorov <petodorov@gmail.com>
2017-06-10 11:21:41 +0300

    E 6

---
+++ Methods-and-Debugging/6. Math Power/Math_Power.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Math_Power
+{
+    class Math_Power
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = CalculatePower(n, power);
+            Console.WriteLine(result);
+        }
+
+        private static double CalculatePower(double n, int power)
+        {
+            double powered = Math.Pow(n, power);
+            return powered;
+        }
+    }
+}
