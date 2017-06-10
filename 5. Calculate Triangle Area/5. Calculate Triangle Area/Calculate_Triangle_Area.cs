bd168d48cad7ad5cdafe4cda2b22f16c85529be2
petertodorov <petodorov@gmail.com>
2017-06-10 11:05:43 +0300

    E 5

---
+++ 5. Calculate Triangle Area/5. Calculate Triangle Area/Calculate_Triangle_Area.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Calculate_Triangle_Area
+{
+    class Calculate_Triangle_Area
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double result = CalculateArea(a, b);
+            Console.WriteLine(result);
+        }
+        private static double CalculateArea(int a, int b)
+        {
+            double area= (a * b) / 2D;
+            return area;
+        }
+    }
+}
