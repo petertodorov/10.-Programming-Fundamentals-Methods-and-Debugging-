a7e46204dc5d73527d5ec25f4fe18b7dcbd453f0
petertodorov <petodorov@gmail.com>
2017-06-10 10:18:09 +0300

    E 1 Does not work don't know why

---
+++ 1. BlankReceipt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class PrintingTriangle
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding =  System.Text.Encoding.Unicode;
+        PrintReceipt();
+    }
+
+    private static void PrintReceipt()
+    {
+        PrintHeader();
+        PrintBody();
+        PrintFooter();
+    }
+
+    private static void PrintFooter()
+    {
+        Console.WriteLine("------------------------------\n\u00A9 SoftUni");
+    }
+
+    private static void PrintBody()
+    {
+        Console.WriteLine("Charged to____________________\nReceived by___________________");
+    }
+
+    private static void PrintHeader()
+    {
+        Console.WriteLine("CASH RECEIPT\n------------------------------");
+    }
+}
+   
